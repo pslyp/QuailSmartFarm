11405211b3eaae5a968f4e7bc31a3e22d4848e5e
pslyp <nonny_038@hotmail.com>
2019-01-21 09:57:42 +0700

    fix1

---
+++ app/src/main/java/com/pslyp/dev/quailsmartfarm/Configs.java
@@ -0,0 +1,13 @@
+package com.pslyp.dev.quailsmartfarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Configs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configs);
+    }
+}

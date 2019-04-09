2cd9cd1f22ee80f6031a6c5c78b354ab054e8bee
PSlyp <nonny_038@hotmail.com>
2019-04-09 13:02:16 +0700

    fix bluetooth

--- app/src/main/java/com/pslyp/dev/quailsmartfarm/DeviceList_2.java
+++ app/src/main/java/com/pslyp/dev/quailsmartfarm/DeviceList_2.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.pslyp.dev.quailsmartfarm.adapter.DeviceListAdapter;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import me.aflak.bluetooth.Bluetooth;
@@ -17,6 +20,8 @@ import me.aflak.bluetooth.DiscoveryCallback;
 public class DeviceList_2 extends AppCompatActivity implements View.OnClickListener {
 
     private Bluetooth mBluetooth = new Bluetooth(this);
+
+    private ArrayList<BluetoothDevice> deviceArrayList;
 
     private Button mButton;
     private ListView mListView;
@@ -31,15 +36,37 @@ public class DeviceList_2 extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        mBluetooth.onStart();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        mBluetooth.onStop();
+    }
+
+    @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_find :
-                mBluetooth.startScanning();
+                deviceArrayList.clear();
+                if(!mBluetooth.getBluetoothAdapter().isDiscovering()) {
+                    mBluetooth.startScanning();
+                } else {
+                    mBluetooth.stopScanning();
+                    mBluetooth.startScanning();
+                }
                 break;
         }
     }
 
     private void initInstance() {
+        deviceArrayList = new ArrayList<>();
+
         mButton = findViewById(R.id.button_find);
         findViewById(R.id.button_find).setOnClickListener(this);
         mListView = findViewById(R.id.list_view);
@@ -61,9 +88,14 @@ public class DeviceList_2 extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onDeviceFound(BluetoothDevice device) {
-//                List<BluetoothDevice> devices = device;
-//
-//                mListView.setAdapter(new ArrayAdapter<BluetoothDevice>(this, R.layout.device_item, device));
+                if(device != null) {
+                    deviceArrayList.add(device);
+                    DeviceListAdapter adapter = new DeviceListAdapter(DeviceList_2.this, R.layout.device_item, deviceArrayList);
+                    mListView.setAdapter(adapter);
+                    mTextView.setVisibility(View.GONE);
+                } else {
+                    mListView.setVisibility(View.GONE);
+                }
             }
 
             @Override
@@ -87,6 +119,7 @@ public class DeviceList_2 extends AppCompatActivity implements View.OnClickListe
         if(mBluetooth.isEnabled()) {
             List<BluetoothDevice> devices = mBluetooth.getPairedDevices();
 
+            mListView.setAdapter(null);
             if(devices != null) {
                 mListView.setAdapter(new ArrayAdapter<BluetoothDevice>(this, R.layout.device_item, devices));
                 mTextView.setVisibility(View.GONE);
